ee860d630e2738cc827d1bb6a883efecc995c92c
mirko <lazarevic.mirko@gmail.com>
2016-10-11 22:56:56 +0200

    MaxCounters

--- countingElements/maxCounters.js
+++ countingElements/maxCounters.js
@@ -2,4 +2,41 @@
  * Created by mirko on 11.10.16..
  */
 
-function solution()
+function solution(N, A) {
+    var results = new Array(N);
+
+    for (var k = 0; k < results.length; k++) {
+        results[k] = 0;
+    }
+
+    var max = 0;
+    var isMaxCounterBefore = false;
+
+    for (var i = 0; i < A.length; i++) {
+        var val = A[i];
+        var index = val - 1;
+
+        if (val <= N) {
+            results[index]++;
+            if (results[index] > max) {
+                max = results[index];
+            }
+            isMaxCounterBefore = false;
+        } else {
+            if (!isMaxCounterBefore) {
+                for (var j = 0; j < results.length; j++) {
+                    results[j] = max;
+                }
+            }
+            isMaxCounterBefore = true;
+        }
+    }
+
+    return results;
+
+}
+
+
+// console.log(solution(5, [3, 4, 4, 6, 1, 4, 4]));
+
+module.exports = solution;
